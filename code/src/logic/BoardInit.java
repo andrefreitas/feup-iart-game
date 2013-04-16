55ac2b31af033942e3313678471920e7c62ebe1a
Daniel Nora <daninora9@gmail.com>
2013-04-16 22:19:58 +0100

    Melhoramentos na classe BoardInit.

--- code/src/logic/BoardInit.java
+++ code/src/logic/BoardInit.java
@@ -5,6 +5,8 @@ package logic;
  */
 public class BoardInit 
 {
+	//TODO: compactar código de iniciação das adjacências
+	
 	/**
 	 * Método que invoca as funções adequadas para inicializar um tabuleiro recebido como argumento.
 	 * @param board Objeto Board que se pretende inicializar.
@@ -197,172 +199,48 @@ public class BoardInit
 	 */
 	private static void initMills(Board board) 
 	{
-		SlotMills a0=new SlotMills();
-		a0.mill1.add("3-0");
-		a0.mill1.add("6-0");
-		a0.mill2.add("0-3");
-		a0.mill2.add("0-6");
-		board.millsMap.put("0-0",a0);
-
-		SlotMills a1=new SlotMills();
-		a1.mill1.add("0-0");
-		a1.mill1.add("0-6");
-		a1.mill2.add("1-3");
-		a1.mill2.add("2-3");
-		board.millsMap.put("0-3",a1);
-
-		SlotMills a2=new SlotMills();
-		a2.mill1.add("0-0");
-		a2.mill1.add("0-3");
-		a2.mill2.add("3-6");
-		a2.mill2.add("6-6");
-		board.millsMap.put("0-6",a2);
-
-		SlotMills a3=new SlotMills();
-		a3.mill1.add("0-0");
-		a3.mill1.add("6-0");
-		a3.mill2.add("3-1");
-		a3.mill2.add("3-2");
-		board.millsMap.put("3-0",a3);
-
-		SlotMills a4=new SlotMills();
-		a4.mill1.add("0-0");
-		a4.mill1.add("3-0");
-		a4.mill2.add("6-3");
-		a4.mill2.add("6-6");
-		board.millsMap.put("6-0",a4);
-
-		SlotMills a5=new SlotMills();
-		a5.mill1.add("6-0");
-		a5.mill1.add("6-6");
-		a5.mill2.add("5-3");
-		a5.mill2.add("4-3");
-		board.millsMap.put("6-3",a5);
-
-		SlotMills a6=new SlotMills();
-		a6.mill1.add("6-0");
-		a6.mill1.add("6-3");
-		a6.mill2.add("3-6");
-		a6.mill2.add("0-6");
-		board.millsMap.put("6-6",a6);
-
-		SlotMills a7=new SlotMills();
-		a7.mill1.add("6-6");
-		a7.mill1.add("0-6");
-		a7.mill2.add("3-4");
-		a7.mill2.add("3-5");
-		board.millsMap.put("3-6",a7);
-
-		SlotMills a8=new SlotMills();
-		a8.mill1.add("1-3");
-		a8.mill1.add("1-5");
-		a8.mill2.add("3-1");
-		a8.mill2.add("5-1");
-		board.millsMap.put("1-1",a8);
-
-		SlotMills a9=new SlotMills();
-		a9.mill1.add("1-1");
-		a9.mill1.add("1-5");
-		a9.mill2.add("0-3");
-		a9.mill2.add("2-3");
-		board.millsMap.put("1-3",a9);
-
-		SlotMills a10=new SlotMills();
-		a10.mill1.add("1-1");
-		a10.mill1.add("1-3");
-		a10.mill2.add("3-5");
-		a10.mill2.add("5-5");
-		board.millsMap.put("1-5",a10);
-
-		SlotMills a11=new SlotMills();
-		a11.mill1.add("1-1");
-		a11.mill1.add("5-1");
-		a11.mill2.add("3-0");
-		a11.mill2.add("3-2");
-		board.millsMap.put("3-1",a11);
-
-		SlotMills a12=new SlotMills();
-		a12.mill1.add("3-1");
-		a12.mill1.add("1-1");
-		a12.mill2.add("5-3");
-		a12.mill2.add("5-5");
-		board.millsMap.put("5-1",a12);
-
-		SlotMills a13=new SlotMills();
-		a13.mill1.add("4-3");
-		a13.mill1.add("6-3");
-		a13.mill2.add("5-1");
-		a13.mill2.add("5-5");
-		board.millsMap.put("5-3",a13);
-
-		SlotMills a14=new SlotMills();
-		a14.mill1.add("5-1");
-		a14.mill1.add("5-3");
-		a14.mill2.add("3-5");
-		a14.mill2.add("1-5");
-		board.millsMap.put("5-5",a14);
-
-		SlotMills a15=new SlotMills();
-		a15.mill1.add("1-5");
-		a15.mill1.add("5-5");
-		a15.mill2.add("3-4");
-		a15.mill2.add("3-6");
-		board.millsMap.put("3-5",a15);
-
-		SlotMills a16=new SlotMills();
-		a16.mill1.add("3-2");
-		a16.mill1.add("4-2");
-		a16.mill2.add("2-3");
-		a16.mill2.add("2-4");
-		board.millsMap.put("2-2",a16);
-
-		SlotMills a17=new SlotMills();
-		a17.mill1.add("2-2");
-		a17.mill1.add("2-4");
-		a17.mill2.add("0-3");
-		a17.mill2.add("1-3");
-		board.millsMap.put("2-3",a17);
-
-		SlotMills a18=new SlotMills();
-		a18.mill1.add("2-2");
-		a18.mill1.add("2-3");
-		a18.mill2.add("3-4");
-		a18.mill2.add("4-4");
-		board.millsMap.put("2-4",a18);
-
-		SlotMills a19=new SlotMills();
-		a19.mill1.add("2-2");
-		a19.mill1.add("4-2");
-		a19.mill2.add("3-1");
-		a19.mill2.add("3-0");
-		board.millsMap.put("3-2",a19);
-
-		SlotMills a20=new SlotMills();
-		a20.mill1.add("3-2");
-		a20.mill1.add("2-2");
-		a20.mill2.add("4-3");
-		a20.mill2.add("4-4");
-		board.millsMap.put("4-2",a20);
-
-		SlotMills a21=new SlotMills();
-		a21.mill1.add("4-2");
-		a21.mill1.add("4-4");
-		a21.mill2.add("5-3");
-		a21.mill2.add("6-3");
-		board.millsMap.put("4-3",a21);
-
-		SlotMills a22=new SlotMills();
-		a22.mill1.add("4-2");
-		a22.mill1.add("4-3");
-		a22.mill2.add("3-4");
-		a22.mill2.add("2-4");
-		board.millsMap.put("4-4",a22);
-
-		SlotMills a23=new SlotMills();
-		a23.mill1.add("2-4");
-		a23.mill1.add("4-4");
-		a23.mill2.add("3-5");
-		a23.mill2.add("3-6");
-		board.millsMap.put("3-4",a23);
+		putMill(board,"0-0","3-0","6-0","0-3","0-6");
+		putMill(board,"0-3","0-0","0-6","1-3","2-3");
+		putMill(board,"0-6","0-0","0-3","3-6","6-6");
+		putMill(board,"3-0","0-0","6-0","3-1","3-2");
+		putMill(board,"6-0","0-0","3-0","6-3","6-6");
+		putMill(board,"6-3","6-0","6-6","5-3","4-3");
+		putMill(board,"6-6","6-0","6-3","3-6","0-6");
+		putMill(board,"3-6","6-6","0-6","3-4","3-5");
+		putMill(board,"1-1","1-3","1-5","3-1","5-1");
+		putMill(board,"1-3","1-1","1-5","0-3","2-3");
+		putMill(board,"1-5","1-1","1-3","3-5","5-5");
+		putMill(board,"3-1","1-1","5-1","3-0","3-2");
+		putMill(board,"5-1","3-1","1-1","5-3","5-5");
+		putMill(board,"5-3","4-3","6-3","5-1","5-5");
+		putMill(board,"5-5","5-1","5-3","3-5","1-5");
+		putMill(board,"3-5","1-5","5-5","3-4","3-6");
+		putMill(board,"2-2","3-2","4-2","2-3","2-4");
+		putMill(board,"2-3","2-2","2-4","0-3","1-3");
+		putMill(board,"2-4","2-2","2-3","3-4","4-4");
+		putMill(board,"3-2","2-2","4-2","3-1","3-0");
+		putMill(board,"4-2","3-2","2-2","4-3","4-4");
+		putMill(board,"4-3","4-2","4-4","5-3","6-3");
+		putMill(board,"4-4","4-2","4-3","3-4","2-4");
+		putMill(board,"3-4","2-4","4-4","3-5","3-6");
+	}
+	
+	/**
+	 * Método auxiliar que permite introduzir informação sobre os mills (sempre 2) nos quais uma slot está inserida
+	 * @param board Objeto Board que se pretende inicializar.
+	 * @param slot Identificador da slot cujos mills na qual se insere são gerados
+	 * @param m1a1 Slot 1 do mill 1
+	 * @param m1a2 Slot 2 do mill 1
+	 * @param m2a1 Slot 1 do mill 2
+	 * @param m2a2 Slot 2 do mill 2
+	 */
+	private static void putMill(Board board, String slot, String m1a1, String m1a2, String m2a1, String m2a2)
+	{
+		SlotMills a=new SlotMills();
+		a.mill1.add(m1a1);
+		a.mill1.add(m1a2);
+		a.mill2.add(m2a1);
+		a.mill2.add(m2a2);
+		board.millsMap.put(slot, a);
 	}
 }
